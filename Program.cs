98e6c4c6f40a8d068a44674cab97ddea43facdae
emirgunes01 <155913729+emirgunes01@users.noreply.github.com>
2025-05-08 21:12:44 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace HaftaninGunleri
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Pazartesi");
+            Console.WriteLine("Salı");
+            Console.WriteLine("Çarşamba");
+            Console.WriteLine("Perşembe");
+            Console.WriteLine("Cuma");
+            Console.WriteLine("Cumartesi");
+            Console.WriteLine("Pazar");
+
+            Console.ReadLine();
+        }
+    }
+}
